c998240a9cf8cbb152622a962b21339a07a8ccc8
achraf bilal <bilal_achraf@emsi-edu.ma>
2022-09-16 12:19:47 +0100

    Add employee modal

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { EmployeeService } from './employee.service';
 import { Employee } from './employee';
 import { Component } from '@angular/core';
 import { HttpErrorResponse } from '@angular/common/http';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -18,10 +19,40 @@ export class AppComponent {
     this.employeeService.getEmployees().subscribe(
       (response: Employee[]) => {
         this.employees = response;
+        console.log(response);
       },
       (err: HttpErrorResponse) => {
         alert(err.message);
       }
     );
+  }
+  public onAddEmployee(addForm: NgForm): void {
+    console.log(addForm.value);
+  }
+
+  public onOpenModal(mode: String, employee?: Employee): void {
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+
+    let target = '';
+    console.log(employee);
+
+    switch (mode) {
+      case 'add':
+        target = 'addEmployeeModal';
+        break;
+      case 'edit':
+        target = 'editEmployeeModal';
+        break;
+      case 'delete':
+        target = 'deleteEmployeeModal';
+        break;
+    }
+    button.setAttribute('data-target', `#${target}`);
+    container?.appendChild(button);
+    button.click();
   }
 }
--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, FormsModule],
   providers: [EmployeeService],
   bootstrap: [AppComponent],
 })
